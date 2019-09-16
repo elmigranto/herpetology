9925e9260ee8ea77f4bb673f3bb40cc425293455
Aleksei Zabrodskii <elmigranto@protonmail.com>
2019-09-16 02:16:53 +0200

    Ah… Much better heredoc!

--- src/utils/heredoc.py
+++ src/utils/heredoc.py
@@ -1,33 +1,12 @@
-from sys import maxsize as _fake_infinity
+from textwrap import dedent
 
 
 def heredoc(multiline: str) -> str:
-    lines = multiline.rstrip().strip('\n').split('\n')
-    characters_to_trim = min(map(_number_of_leading_space, lines))
-
-    return '\n'.join(map(
-        lambda x: x[characters_to_trim:].rstrip(),
-        lines
-    ))
+    return dedent(multiline).strip()
 
 
 def single_line(multiline: str) -> str:
     return ' '.join(filter(
-        lambda s: len(s) > 0,
-        map(str.strip, heredoc(multiline).strip().split('\n'))
+        len,
+        map(str.strip, multiline.splitlines())
     ))
-
-
-def _number_of_leading_space(string: str) -> int:
-    amount = 0
-
-    if string == '':
-        return _fake_infinity
-
-    for char in string:
-        if char == ' ':
-            amount = amount + 1
-        else:
-            break
-
-    return amount
--- src/utils/heredoc_test.py
+++ src/utils/heredoc_test.py
@@ -3,16 +3,16 @@ from utils.heredoc import heredoc, single_line
 
 
 class TestUtilsHeredocHeredoc(TestCase):
-    def test_empty_string_stays_the_same(self) -> None:
+    def test_it_keeps_empty_strings_empty(self) -> None:
         self.assertEqual(heredoc(''), '')
 
-    def test_single_line_is_trimmed(self) -> None:
+    def test_it_trims_the_result(self) -> None:
         self.assertEqual(
             heredoc(' Hello, dear friends!\n'),
             'Hello, dear friends!'
         )
 
-    def test_multiline_trims_least_number_of_leading_spaces(self) -> None:
+    def test_it_dedents_common_leading_whitespace(self) -> None:
         self.assertEqual(
             heredoc('''
                 Oh wow!
@@ -31,23 +31,50 @@ class TestUtilsHeredocHeredoc(TestCase):
             ))
         )
 
+    def test_it_ignores_empty_lines_on_both_ends(self):
+        self.assertEqual(
+            heredoc('''
+
+
+                all above
+
+                not middle ones
+
+                all below
+
+
+            '''),
+            'all above\n\nnot middle ones\n\nall below'
+        )
+
 
 class TestUtilsHeredocSingleLine(TestCase):
-    def test_empty_string_stays_the_same(self) -> None:
+    def test_it_keeps_empty_strings_empty(self) -> None:
         self.assertEqual(single_line(''), '')
 
-    def test_single_line_is_trimmed(self) -> None:
+    def test_it_trims_the_result(self) -> None:
         self.assertEqual(single_line(' How nice! \n'), 'How nice!')
 
-    def test_multiline_is_converted_to_single_line(self) -> None:
+    def test_it_multiline_becomes_single_line(self):
         self.assertEqual(
             single_line('''
-                Oh wow!
-                  banana
-
-                 It's nice!
-                    So…
+                Hello, dear friend!
+                How are you doing?
+                Hope all is well.
             '''),
+            'Hello, dear friend! How are you doing? Hope all is well.'
+        )
 
-            "Oh wow! banana It's nice! So…"
+    def test_it_replace_multiple_newlines_with_one_space(self) -> None:
+        self.assertEqual(
+            single_line('''
+                Inline\t\tmultispace:  <-- is kept.
+
+
+                But multiple empty lines like above
+                  and this indent to the left
+                   are collapsed
+                    into one!
+            '''),
+            "Inline\t\tmultispace:  <-- is kept. But multiple empty lines like above and this indent to the left are collapsed into one!"
         )
